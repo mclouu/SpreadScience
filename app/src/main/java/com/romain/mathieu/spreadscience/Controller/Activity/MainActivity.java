a24438f77315bacade3a8cd4ca8d4e5a634e7cad
romain <rmathieu13@gmail.com>
2018-03-16 11:39:13 +0100

    Adding ViewPager and Menu

---
+++ app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/MainActivity.java
@@ -0,0 +1,71 @@
+package com.romain.mathieu.spreadscience.Controller.Activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.romain.mathieu.spreadscience.R;
+import com.romain.mathieu.spreadscience.View.PageAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.configureViewPagerAndTabs();
+    }
+
+
+    //-------------
+    // VIEW PAGER
+    //-------------
+
+    private void configureViewPagerAndTabs() {
+        // 1 - Get ViewPager from layout
+        ViewPager pager = findViewById(R.id.activity_main_viewpager);
+        // 1 - Set Adapter PageAdapter and glue it together
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager(), getResources().getIntArray(R.array.colorPagersViewPager)));
+
+        // 2 - Get TabLayout from layout
+        TabLayout tabs = findViewById(R.id.activity_main_tabs);
+        // 2 - Glue TabLayout and ViewPager together
+        tabs.setupWithViewPager(pager);
+        // 3 - Tabs have the same width
+        tabs.setTabMode(TabLayout.MODE_FIXED);
+
+    }
+
+
+    //------------
+    // MENU
+    //------------
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_notif:
+
+                return true;
+            case R.id.menu_help:
+
+                return true;
+            case R.id.menu_about:
+
+                return true;
+            case R.id.menu_search:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
