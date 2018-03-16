63027e95d774fd14830035e7241e309abe316652
romain <rmathieu13@gmail.com>
2018-03-17 00:50:52 +0100

    CReate toolBar

--- app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/MainActivity.java
+++ app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("gddgdg");
 
         this.configureViewPagerAndTabs();
     }
@@ -38,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
         //Design purpose. Tabs have the same width
         tabs.setTabMode(TabLayout.MODE_FIXED);
     }
+
+
+    //------------
+    // TOOLBAR
+    //------------
 
 
     //------------
