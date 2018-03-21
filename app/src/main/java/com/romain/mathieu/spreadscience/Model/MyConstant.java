50e819ec97d4142498a983726be04a64336a9326
romain <rmathieu13@gmail.com>
2018-03-21 05:58:15 +0100

    create file MyConstant for my constant

---
+++ app/src/main/java/com/romain/mathieu/spreadscience/Model/MyConstant.java
@@ -0,0 +1,11 @@
+package com.romain.mathieu.spreadscience.Model;
+
+/**
+ * Created by romain on 19/03/2018.
+ */
+
+public class MyConstant {
+
+    public String BASE_URL = "http://long-pixel.alwaysdata.net";
+
+}
