ec19a347f19b762909ffab1388c014d82574977d
romain <rmathieu13@gmail.com>
2018-03-21 05:55:50 +0100

    Adding TabLayout and VewPager

--- app/src/main/java/com/romain/mathieu/spreadscience/View/MyAdapter.java
+++ app/src/main/java/com/romain/mathieu/spreadscience/View/MyAdapter.java
@@ -1,14 +1,17 @@
 package com.romain.mathieu.spreadscience.View;
 
-import android.support.v7.widget.CardView;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.romain.mathieu.spreadscience.Model.CardViewData;
+import com.bumptech.glide.Glide;
+import com.romain.mathieu.spreadscience.Model.CardData;
 import com.romain.mathieu.spreadscience.R;
 
 import java.util.ArrayList;
@@ -17,61 +20,59 @@ import java.util.ArrayList;
  * Created by romain on 17/03/2018.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.NewsViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter {
+
+    static Context context;
+
+    private ArrayList<CardData> dataset;
 
 
-    private ArrayList<CardViewData> mdatas;
-
-    public MyAdapter(ArrayList<CardViewData> cardViewData) {
-        this.mdatas = cardViewData;
+    public MyAdapter(ArrayList<CardData> mlist) {
+        this.dataset = mlist;
     }
 
+    public static class ImageTypeViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public NewsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-        return new NewsViewHolder(v);
+
+        TextView title, subtitle;
+        ImageView imageView;
+
+        ImageTypeViewHolder(View itemView) {
+            super(itemView);
+
+            context = itemView.getContext();
+            this.title = itemView.findViewById(R.id.title);
+            this.subtitle = itemView.findViewById(R.id.content);
+            this.imageView = itemView.findViewById(R.id.thumbnail);
+        }
     }
 
     @Override
-    public void onBindViewHolder(NewsViewHolder holder, int position) {
-        final CardViewData item = mdatas.get(position);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ImageTypeViewHolder(view);
+    }
 
-        holder.mtitle.setText(item.title);
-        holder.mcontent.setText(item.content);
-        holder.mthumbnail.setImageResource(item.imageCard);
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        final CardData object = dataset.get(position);
 
+
+        ((ImageTypeViewHolder) holder).title.setText(Html.fromHtml(object.getTitle()));
+        ((ImageTypeViewHolder) holder).subtitle.setText(Html.fromHtml(object.getSubtitle()));
+
+        String url = object.getImageURL();
+        Log.e(" Adapater URl image : ", url);
+        Glide.with(context)
+                .load(url)
+                .into(((ImageTypeViewHolder) holder).imageView);
+
+
+        /// dataset.get(position)
     }
 
     @Override
     public int getItemCount() {
-        return mdatas.size();
+        return dataset.size();
     }
-
-
-    class NewsViewHolder extends RecyclerView.ViewHolder {
-
-        CardView mcardView;
-        ImageView mthumbnail;
-        TextView mtitle;
-        TextView mcontent;
-
-        NewsViewHolder(final View itemView) {
-            super(itemView);
-            // findViewById ------------------------------------------------------------
-            mcardView = itemView.findViewById(R.id.card_view);
-            mthumbnail = itemView.findViewById(R.id.thumbnail);
-            mtitle = itemView.findViewById(R.id.title);
-            mcontent = itemView.findViewById(R.id.content);
-            // findViewById ------------------------------------------------------------
-
-            mcardView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-        }
-    }
-
 }
