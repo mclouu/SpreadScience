d5a22210d429e1227f420a4f2ac9d01021e32052
romain <rmathieu13@gmail.com>
2018-04-04 09:10:43 +0200

    putExtra title and content positiion

--- app/src/main/java/com/romain/mathieu/spreadscience/View/MyAdapter.java
+++ app/src/main/java/com/romain/mathieu/spreadscience/View/MyAdapter.java
@@ -1,8 +1,12 @@
 package com.romain.mathieu.spreadscience.View;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.romain.mathieu.spreadscience.Controller.Activity.ArticleActivity;
 import com.romain.mathieu.spreadscience.Model.CardData;
 import com.romain.mathieu.spreadscience.R;
 
@@ -19,63 +24,88 @@ import java.util.ArrayList;
  * Created by romain on 17/03/2018.
  */
 
-public class MyAdapter extends RecyclerView.Adapter {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ArticleViewHolder> {
 
-    static Context context;
+    private Context context;
 
-    private ArrayList<CardData> dataset;
+    public MyAdapter(Context context) {
+        this.context = context;
+    }
+
+    private ArrayList<CardData> mdatas;
 
 
     public MyAdapter(ArrayList<CardData> mlist) {
-        this.dataset = mlist;
+        this.mdatas = mlist;
     }
 
-    public static class ImageTypeViewHolder extends RecyclerView.ViewHolder {
-
-
-        TextView title, subtitle, date, category;
-        ImageView imageView;
-
-        ImageTypeViewHolder(View itemView) {
-            super(itemView);
-
-            context = itemView.getContext();
-            this.title = itemView.findViewById(R.id.title);
-            this.subtitle = itemView.findViewById(R.id.content);
-            this.imageView = itemView.findViewById(R.id.thumbnail);
-            this.date = itemView.findViewById(R.id.date);
-            this.category = itemView.findViewById(R.id.category);
-        }
-    }
-
+    @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ArticleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-        return new ImageTypeViewHolder(view);
+        return new ArticleViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
-        final CardData object = dataset.get(position);
+    public void onBindViewHolder(@NonNull ArticleViewHolder holder, int position) {
+        final CardData object = mdatas.get(position);
 
 
-        ((ImageTypeViewHolder) holder).title.setText(Html.fromHtml(object.getTitle()));
-        ((ImageTypeViewHolder) holder).subtitle.setText(Html.fromHtml(object.getSubtitle()));
-        ((ImageTypeViewHolder) holder).date.setText(object.getDate());
-        ((ImageTypeViewHolder) holder).category.setText(object.getCategory());
+        holder.title.setText(Html.fromHtml(object.getTitle()));
+        holder.subtitle.setText(Html.fromHtml(object.getSubtitle()));
+        holder.date.setText(object.getDate());
+        holder.category.setText(object.getCategory());
 
         String url = object.getImageURL();
 
         Glide.with(context)
                 .load(url)
-                .into(((ImageTypeViewHolder) holder).imageView);
-
-
-        /// dataset.get(position)
+                .into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return dataset.size();
+        return mdatas.size();
+    }
+
+
+    class ArticleViewHolder extends RecyclerView.ViewHolder {
+
+
+        CardView cardView;
+        ImageView imageView;
+        TextView title, subtitle, date, category;
+
+
+        ArticleViewHolder(final View itemView) {
+            super(itemView);
+
+            context = itemView.getContext();
+
+            this.cardView = itemView.findViewById(R.id.card_view);
+            this.imageView = itemView.findViewById(R.id.thumbnail);
+            this.title = itemView.findViewById(R.id.title);
+            this.subtitle = itemView.findViewById(R.id.content);
+            this.date = itemView.findViewById(R.id.date);
+            this.category = itemView.findViewById(R.id.category);
+
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+
+
+                    Intent intent = new Intent(context, ArticleActivity.class);
+                    final CardData object = mdatas.get(position);
+                    intent.putExtra("titleArticle", String.valueOf(Html.fromHtml(object.getTitle())));
+                    intent.putExtra("subtitleArticle", String.valueOf(Html.fromHtml(object.getSubtitle())));
+                    intent.putExtra("contentArticle", object.getContent());
+                    Log.e("TAG2", object.getContent());
+                    context.startActivity(intent);
+                }
+            });
+
+
+        }
     }
 }
