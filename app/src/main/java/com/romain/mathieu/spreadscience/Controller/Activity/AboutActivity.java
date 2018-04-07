3553938438620535aedd137337020f4ff5747a07
romain <rmathieu13@gmail.com>
2018-04-07 21:43:08 +0200

    create file

---
+++ app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/AboutActivity.java
@@ -0,0 +1,19 @@
+package com.romain.mathieu.spreadscience.Controller.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.romain.mathieu.spreadscience.R;
+
+/**
+ * Created by romain on 06/04/2018.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
