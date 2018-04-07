c95754328bb6abf6ae6f3eadd8b16e6c2d7b4fe5
romain <rmathieu13@gmail.com>
2018-04-07 21:39:03 +0200

    -

--- app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/MainActivity.java
+++ app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/MainActivity.java
@@ -1,27 +1,50 @@
 package com.romain.mathieu.spreadscience.Controller.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.annotation.NonNull;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.romain.mathieu.spreadscience.Model.API.WPPostAPI;
+import com.romain.mathieu.spreadscience.Model.CardData;
+import com.romain.mathieu.spreadscience.Model.SharedPeferencesUtils;
+import com.romain.mathieu.spreadscience.Model.WordPressService;
 import com.romain.mathieu.spreadscience.R;
-import com.romain.mathieu.spreadscience.View.PageAdapter;
+import com.romain.mathieu.spreadscience.View.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private LinearLayoutManager llm;
+    private MyAdapter adapter;
+    public static ArrayList<CardData> list = new ArrayList<>();
+    public static List<WPPostAPI> responseBody;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
-    @BindView(R.id.activity_main_viewpager)
-    ViewPager pager;
-    @BindView(R.id.activity_main_tabs)
-    TabLayout tabs;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout swipeRefreshLayout;
 
 
     @Override
@@ -31,33 +54,82 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
+        swipeRefreshLayout.setOnRefreshListener(this);
+
         setSupportActionBar(toolbar);
         toolbar.setTitle("toolBar");
 
-        this.configureViewPagerAndTabs();
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView = findViewById(R.id.recyclerView);
+        llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+
+        adapter = new MyAdapter(list);
+        recyclerView.setAdapter(adapter);
+        getRetrofit();
+
     }
 
+    @Override
+    public void onRefresh() {
 
-    //-------------
-    // VIEW PAGER
-    //-------------
+        swipeRefreshLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getRetrofit();
+                list.clear();
+                adapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 2000);
 
-    private void configureViewPagerAndTabs() {
-        //Get ViewPager from layout
-        //Set Adapter PageAdapter and glue it together
-        pager.setAdapter(new PageAdapter(getSupportFragmentManager()));
-
-        //Get TabLayout from layout
-        //Glue TabLayout and ViewPager together
-        tabs.setupWithViewPager(pager);
-        //Design purpose. Tabs have the same width
-        tabs.setTabMode(TabLayout.MODE_FIXED);
     }
 
+    //-----------------------
+    // HTTP REQUEST RETROFIT
+    //-----------------------
 
-    //------------
-    // TOOLBAR
-    //------------
+    public void getRetrofit() {
+
+        WordPressService service = WordPressService.retrofit.create(WordPressService.class);
+
+
+        Call<List<WPPostAPI>> call = service.getPostInfo();
+        call.enqueue(new Callback<List<WPPostAPI>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<WPPostAPI>> call, @NonNull Response<List<WPPostAPI>> response) {
+                responseBody = response.body();
+                progressBar.setVisibility(View.GONE);
+
+                for (int i = 0; i < response.body().size(); i++) {
+
+                    String title = responseBody.get(i).getTitle().getRendered();
+                    String subTitle = responseBody.get(i).getExcerpt().getRendered();
+                    String content = responseBody.get(i).getContent().getRendered();
+                    String featuredImage = responseBody.get(i).getEmbedded().getWpFeaturedmedia().get(0).getSourceUrl();
+                    String date = responseBody.get(i).getDate();
+                    date = date.replace("T", " - ");
+                    String category = responseBody.get(i).getEmbedded().getWpTerm().get(0).get(0).getName();
+                    String url = responseBody.get(i).getLink();
+
+
+                    list.add(new CardData(title, subTitle, featuredImage, date, category, content, url));
+
+
+                }
+                adapter.notifyDataSetChanged();
+                SharedPeferencesUtils.saveArrayList(MainActivity.this);
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<WPPostAPI>> call, @NonNull Throwable t) {
+                Toast.makeText(MainActivity.this, "Lecture hors-ligne", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+
+            }
+        });
+
+    }
 
 
     //------------
@@ -72,14 +144,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_notif:
-
-                return true;
-            case R.id.menu_help:
-
+            case R.id.menu_setting:
                 return true;
             case R.id.menu_about:
-
+                Intent intent2 = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent2);
                 return true;
             case R.id.menu_search:
 
