f2fb238b2dde9721db5be3f41f6cc4f261ba0d33
romain <rmathieu13@gmail.com>
2018-04-07 21:42:13 +0200

    adding Glid lib

--- app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/ArticleActivity.java
+++ app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/ArticleActivity.java
@@ -2,15 +2,23 @@ package com.romain.mathieu.spreadscience.Controller.Activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.romain.mathieu.spreadscience.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.bumptech.glide.request.RequestOptions.*;
 
 /**
  * Created by romain on 31/03/2018.
@@ -18,61 +26,77 @@ import com.romain.mathieu.spreadscience.R;
 
 public class ArticleActivity extends AppCompatActivity {
 
-    TextView mtitle, mcontent;
+    @BindView(R.id.articile_title)
+    TextView mtitle;
+    @BindView(R.id.article_content)
+    TextView mcontent;
+    @BindView(R.id.image_url)
+    ImageView mimageUrl;
+
+
     Context context;
+
+    String title = "";
+    String content = "";
+    String url = "";
+    String imageUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+        ButterKnife.bind(this);
 
         context = getApplicationContext();
 
-        mtitle = findViewById(R.id.articile_title);
-        mcontent = findViewById(R.id.article_content);
 
         Intent intent = getIntent();
         if (intent != null) {
 
-            String title = "";
+
             if (intent.hasExtra("titleArticle")) {
                 title = intent.getStringExtra("titleArticle");
             }
 
-            String content = "";
+
             if (intent.hasExtra("contentArticle")) {
                 content = intent.getStringExtra("contentArticle");
                 Log.e("TAG", content);
             }
 
+
+            if (intent.hasExtra("urlArticle")) {
+                url = intent.getStringExtra("urlArticle");
+            }
+
+            if (intent.hasExtra("imageUrl")) {
+                imageUrl = intent.getStringExtra("imageUrl");
+            }
+
             mtitle.setText(title);
-            mcontent.setText(Html.fromHtml(content, new Html.ImageGetter() {
-                @Override
-                public Drawable getDrawable(String source) {
-                    Drawable drawable;
-                    int dourceId =
-                            getApplicationContext()
-                                    .getResources()
-                                    .getIdentifier(source, "drawable", getPackageName());
+            mcontent.setText(Html.fromHtml(content));
 
-                    drawable =
-                            getApplicationContext()
-                                    .getResources()
-                                    .getDrawable(dourceId);
 
-                    drawable.setBounds(
-                            0,
-                            0,
-                            drawable.getIntrinsicWidth(),
-                            drawable.getIntrinsicHeight());
+            Glide.with(ArticleActivity.this)
+                    .load(imageUrl)
+                    .apply(centerCropTransform())
+                    .into(mimageUrl);
 
-                    return drawable;
-                }
-
-            }, null));
 
             mcontent.setMovementMethod(LinkMovementMethod.getInstance());
 
+
         }
+
+        Button button = findViewById(R.id.btn_article_url);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ArticleActivity.this, WebViewActivity.class);
+                intent.putExtra("urlArticle", url);
+                startActivity(intent);
+            }
+        });
+
     }
 }
