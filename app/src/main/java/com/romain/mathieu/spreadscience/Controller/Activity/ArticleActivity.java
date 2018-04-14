9ec5b1e3802527acd439f462a3f1a26d6e1768cd
romain <rmathieu13@gmail.com>
2018-04-14 13:04:59 +0200

    now, the app can display image (webview)

--- app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/ArticleActivity.java
+++ app/src/main/java/com/romain/mathieu/spreadscience/Controller/Activity/ArticleActivity.java
@@ -2,23 +2,21 @@ package com.romain.mathieu.spreadscience.Controller.Activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.romain.mathieu.spreadscience.R;
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static com.bumptech.glide.request.RequestOptions.*;
+import butterknife.OnClick;
 
 /**
  * Created by romain on 31/03/2018.
@@ -32,6 +30,8 @@ public class ArticleActivity extends AppCompatActivity {
     TextView mcontent;
     @BindView(R.id.image_url)
     ImageView mimageUrl;
+    @BindView(R.id.test)
+    WebView webView;
 
 
     Context context;
@@ -61,7 +61,6 @@ public class ArticleActivity extends AppCompatActivity {
 
             if (intent.hasExtra("contentArticle")) {
                 content = intent.getStringExtra("contentArticle");
-                Log.e("TAG", content);
             }
 
 
@@ -74,12 +73,10 @@ public class ArticleActivity extends AppCompatActivity {
             }
 
             mtitle.setText(title);
-            mcontent.setText(Html.fromHtml(content));
-
-
-            Glide.with(ArticleActivity.this)
+            webView.setBackgroundColor(Color.TRANSPARENT);
+            webView.loadData(content, "text/html; charset=utf-8", "UTF-8");
+            Picasso.get()
                     .load(imageUrl)
-                    .apply(centerCropTransform())
                     .into(mimageUrl);
 
 
@@ -87,16 +84,13 @@ public class ArticleActivity extends AppCompatActivity {
 
 
         }
+    }
 
-        Button button = findViewById(R.id.btn_article_url);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ArticleActivity.this, WebViewActivity.class);
-                intent.putExtra("urlArticle", url);
-                startActivity(intent);
-            }
-        });
+    @OnClick(R.id.btn_article_url)
+    public void submit(View view) {
+        Intent intent = new Intent(ArticleActivity.this, WebViewActivity.class);
+        intent.putExtra("urlArticle", url);
+        startActivity(intent);
 
     }
 }
